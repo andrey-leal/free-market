9c1f3ae2185829245d68d33df48d826fc30eb5c8
andrey-leal <andrey.ribeiro92@gmail.com>
2021-08-01 20:04:04 -0300

    Application Template and Dependencies

---
+++ sellerapi/src/main/java/com/freemarket/seller/sellerapi/SellerapiApplication.java
@@ -0,0 +1,13 @@
+package com.freemarket.seller.sellerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerapiApplication.class, args);
+	}
+
+}
---
+++ sellerapi/src/test/java/com/freemarket/seller/sellerapi/SellerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freemarket.seller.sellerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
