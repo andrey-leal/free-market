a1e56d785eba55ac55ac2ebd796826309ba49c89
andrey-leal <andrey.ribeiro92@gmail.com>
2021-08-01 22:58:47 -0300

    Interface to SellerService

---
+++ sellerapi/src/main/java/com/freemarket/seller/sellerapi/business/service/SellerService.java
@@ -0,0 +1,7 @@
+package com.freemarket.seller.sellerapi.business.service;
+
+import com.freemarket.seller.sellerapi.business.model.Seller;
+
+public interface SellerService {
+    Seller save(Seller seller);
+}
