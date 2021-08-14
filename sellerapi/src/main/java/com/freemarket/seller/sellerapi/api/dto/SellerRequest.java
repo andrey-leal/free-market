2b77513c0dd973761d4e7c4a403e47cd99b0c971
andrey-leal <andrey.ribeiro92@gmail.com>
2021-08-14 13:45:33 -0300

    OpenApi Schema documented

--- sellerapi/src/main/java/com/freemarket/seller/sellerapi/api/dto/SellerRequest.java
+++ sellerapi/src/main/java/com/freemarket/seller/sellerapi/api/dto/SellerRequest.java
@@ -2,6 +2,7 @@ package com.freemarket.seller.sellerapi.api.dto;
 
 import com.freemarket.seller.sellerapi.business.dto.SellerInput;
 import com.freemarket.seller.sellerapi.business.model.SellerPlan;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import javax.validation.constraints.NotEmpty;
@@ -11,21 +12,28 @@ import javax.validation.constraints.Pattern;
 @Builder
 public class SellerRequest implements SellerInput {
 
+    @Schema(description = "Seller UserName ")
     @NotEmpty(message = "The username must be filled")
     private String userName;
 
+    @Schema(description = "Seller StoreName ")
     @NotEmpty(message = "The storeName must be filled")
     private String storeName;
 
+    @Schema(description = "Seller password ")
     @NotEmpty(message = "The password must be filled")
     private String password;
 
+    @Schema(description = "Seller Email ")
     @NotEmpty(message = "The email must be filled")
     private String email;
 
+    @Schema(description = "Seller Phone Number ")
     @Pattern(regexp="[\\d]", message = "Must be only numbers" )
     private String phoneNumber;
 
+    @Schema(description = "The type of Plan the seller will choose (INDIVIDUAL, STORE, MEGASTORE)",
+           example = "INDIVIDUAL")
     @NotNull
     private SellerPlan sellerPlan;
 
