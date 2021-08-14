88d17a6edc7b6af8cec6608e7cd50f68c086e128
andrey-leal <andrey.ribeiro92@gmail.com>
2021-08-14 15:26:12 -0300

    Component Login API with oauth2

---
+++ login-oauth2/src/main/java/com/freemarket/loginoauth2/LoginOauth2Application.java
@@ -0,0 +1,13 @@
+package com.freemarket.loginoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginOauth2Application.class, args);
+	}
+
+}
---
+++ login-oauth2/src/test/java/com/freemarket/loginoauth2/LoginOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freemarket.loginoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
